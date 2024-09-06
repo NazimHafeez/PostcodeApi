14d9cc610cb0bc5f3c8c5cca987ee46459e3e8ff
Nazim Hafeez <nazim.hafeez@ihsmarkit.com>
2024-09-06 12:04:47 +0100

    WIP: Inial commit for postcode api

---
+++ PostcodeService.cs
@@ -0,0 +1,23 @@
+﻿using MarkEmbling.PostcodesIO;
+using MarkEmbling.PostcodesIO.Results;
+namespace PostcodeApi;
+
+public class PostcodeService : IPostcodeService
+{
+    private readonly IPostcodesIOClient _client;
+
+    public PostcodeService(IPostcodesIOClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<PostcodeResult> LookupAsync(string postcode)
+    {
+        return await _client.LookupAsync(postcode);
+    }
+
+    public async Task<IEnumerable<string>> AutocompleteAsync(string partialPostcode)
+    {
+        return await _client.AutocompleteAsync(partialPostcode);
+    }
+}
